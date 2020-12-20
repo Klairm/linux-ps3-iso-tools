e0b0bee3886ffcfa0a24f76cd9ce398ae4db79cb
Klairm <ivangardis@hotmail.com>
2020-12-20 21:53:34 +0100

    Clear text on new process and optimize the process for extract/create ISO on one function.

--- PS3_ISO_Tools.py
+++ PS3_ISO_Tools.py
@@ -4,7 +4,6 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import (QMainWindow, QTextEdit, 
 	QAction, QFileDialog, QApplication,QMessageBox)
-import time,sys
 
 class Ui_MainWindow(object):
 	def setupUi(self, MainWindow):
@@ -18,20 +17,25 @@ class Ui_MainWindow(object):
 		app_icon.addFile('256x256.png', QtCore.QSize(256,256))
 		
 		app.setWindowIcon(app_icon)
-		self.p = None
+		self.p = None  # Default empty value for the process
+		
+		# Set Widgets  and size, buttons, layout, plaintext...
 		
 		self.centralwidget = QtWidgets.QWidget(MainWindow)
 		self.centralwidget.setObjectName("centralwidget")
+		
 		self.gridLayoutWidget = QtWidgets.QWidget(self.centralwidget)
 		self.gridLayoutWidget.setGeometry(QtCore.QRect(20, 10, 331, 101))
 		self.gridLayoutWidget.setObjectName("gridLayoutWidget")
 		self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
 		self.gridLayout.setContentsMargins(0, 0, 0, 0)
 		self.gridLayout.setObjectName("gridLayout")
+		
 		self.PbExtract = QtWidgets.QPushButton(self.gridLayoutWidget)
 		self.PbExtract.setObjectName("PbExtract")
 		self.gridLayout.addWidget(self.PbExtract, 0, 1, 1, 1)
 		self.PbExtract.clicked.connect(self.extractISO)
+		
 		self.PbConvertISO = QtWidgets.QPushButton(self.gridLayoutWidget)
 		self.PbConvertISO.setObjectName("PbConvertISO")
 		self.gridLayout.addWidget(self.PbConvertISO, 1, 1, 1, 1)
@@ -40,9 +44,8 @@ class Ui_MainWindow(object):
 		self.text = QtWidgets.QPlainTextEdit(self.centralwidget)
 		self.text.setReadOnly(True)
 		self.text.setGeometry(QtCore.QRect(20, 120, 331, 211))
+		
 		MainWindow.setCentralWidget(self.centralwidget)
-		
-		
 		self.retranslateUi(MainWindow)
 		QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
@@ -51,40 +54,38 @@ class Ui_MainWindow(object):
 		MainWindow.setWindowTitle(_translate("MainWindow", "PS3 ISO TOOLS"))
 		self.PbExtract.setText(_translate("MainWindow", "Extract ISO"))
 		self.PbConvertISO.setText(_translate("MainWindow", "Convert to ISO"))
+		
 	def extractISO(self):
+	  self.text.clear()
 	  filename = QFileDialog.getOpenFileName(None, "Open ISO File", '.', "ISO Files (*.iso)")
 	  if filename:
 	   destination =QFileDialog.getExistingDirectory(None, "Select destination folder") 
 	   if destination:
 		   command = "extractps3iso"
 		   arguments = [filename[0],destination]
-		   if self.p is None:
-			   self.message("Wait, Executing process...")
-			   self.p = QtCore.QProcess()
-			   self.p.readyReadStandardOutput.connect(self.handle_stdout)
-			   self.p.readyReadStandardError.connect(self.handle_stderr)
-			   self.p.stateChanged.connect(self.handle_state)
-			   self.p.finished.connect(self.process_finished)
-			   self.p.start(command,arguments)
-			   
-				
+		   self.start_process(command,arguments)
+		
 	def makeISO(self):
+		self.text.clear()
 		game =QFileDialog.getExistingDirectory(None, "Select game folder (example BLES/BLUS)") 
 		if game:
 			destination = QFileDialog.getExistingDirectory(None, "Select destination folder") 
 			if destination:
 				command = "makeps3iso"
 				arguments = [game,destination]
-				if self.p is None:
-					self.message("Wait, Executing process...")
-					self.p = QtCore.QProcess()
-					self.p.readyReadStandardOutput.connect(self.handle_stdout)
-					self.p.readyReadStandardError.connect(self.handle_stderr)
-					self.p.stateChanged.connect(self.handle_state)
-					self.p.finished.connect(self.process_finished)
-					self.p.start(command,arguments)
+				self.start_process(command,arguments)
+			
 
-
+	def start_process(self,command,arguments):
+		if self.p is None:
+			self.message("Wait, Executing process...")
+			self.p = QtCore.QProcess()
+			self.p.readyReadStandardOutput.connect(self.handle_stdout)
+			self.p.readyReadStandardError.connect(self.handle_stderr)
+			self.p.stateChanged.connect(self.handle_state)
+			self.p.finished.connect(self.process_finished)
+			self.p.start(command,arguments)
+	
 	def message(self, s):
 		self.text.appendPlainText(s)
 	
@@ -111,14 +112,7 @@ class Ui_MainWindow(object):
 		self.message("Process finished! You can close the program now.")
 		self.p = None        
 
-	
-		
-		
-		
-				
-				
-		
-
+			
 if __name__ == "__main__":
 	import sys
 	app = QtWidgets.QApplication(sys.argv)
